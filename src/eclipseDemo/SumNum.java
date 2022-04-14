f9121ecec791a4f5a8f873f88403c08fb55b0da5
Rafeek313 <epyshan@gmail.com>
2022-04-14 15:45:00 +0530

    java program

---
+++ src/eclipseDemo/SumNum.java
@@ -0,0 +1,15 @@
+package eclipseDemo;
+
+public class SumNum {
+
+	public static void main(String[] args) {
+		int n=10;
+		int sum=0;
+		for(int i=1;i<n;i++)
+		{
+			sum=i+sum;
+		}
+    System.out.println(sum);
+	}
+
+}
